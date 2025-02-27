d1507cf15d65ae79329190cc5841a840c3937985
Gatt101 <gauravpatilk11@gmail.com>
2025-02-27 23:59:10 +0530

    main commit

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -2,12 +2,19 @@ import { Component } from '@angular/core';
 import { NgxAuroraComponent } from '@omnedia/ngx-aurora';
 import { NgxBorderBeamComponent } from '@omnedia/ngx-border-beam';
 import { NgxTypewriterComponent } from '@omnedia/ngx-typewriter';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgxBorderBeamComponent,NgxAuroraComponent],
+  imports: [NgxBorderBeamComponent,NgxAuroraComponent, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  sentimentText: string = '';
+  
+  analyzeSentiment() {
+    // TODO: Implement sentiment analysis logic
+  }
 }
