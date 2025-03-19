db83bff804f159e2f22040ead13f750c71f7586c
Gatt101 <gauravpatilk11@gmail.com>
2025-03-19 23:18:13 +0530

    added

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,21 @@
 import requests
-from flask import Flask,request,jsonify;
-from flask_cors import CORS;
-from transformers import AutoTokenizer, AutoModelForSequenceClassification;
-import torch;
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
 
 app = Flask(__name__)
 CORS(app)
-
 
 model_name = "tabularisai/multilingual-sentiment-analysis"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 
-@app.route('/',methods=['GET'])
+@app.route('/', methods=['GET'])
 def hello():
     return 'Hello, World!'
 
-@app.route('/analyze_sentiment',methods=['POST'])
+@app.route('/analyze_sentiment', methods=['POST'])
 def analyse_sentiment():
     texts = request.get_json()['text']
     inputs = tokenizer(texts, return_tensors="pt", truncation=True, padding=True, max_length=512)
@@ -26,33 +25,6 @@ def analyse_sentiment():
     sentiment_map = {0: "Very Negative", 1: "Negative", 2: "Neutral", 3: "Positive", 4: "Very Positive"}
     data = [sentiment_map[p] for p in torch.argmax(probabilities, dim=-1).tolist()]
     return jsonify(data)
- 
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-# from flask import Flask, request, jsonify;
-# from flask_cors import CORS;
-
-# app = Flask(__name__)
-# CORS(app)
-
-# @app.route('/get_data', methods=['GET'])
-# def get_data():
-#     data = {
-#         'name': 'John Doe',
-#         'age': 30,
-#         'city': 'New York'
-#     }
-#     return jsonify(data)
-
-# @app.route('/post_data', methods=['POST'])
-# def post_data():
-#     data = request.get_json()
-#     return jsonify(data)
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
