8ae7592bf252094e65cb7bd8bdbbd7443821d169
Gatt101 <gauravpatilk11@gmail.com>
2025-03-25 23:20:04 +0530

    add .

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,10 +1,10 @@
 module.exports = {
-    content: [
-      "./src/**/*.{html,ts}", // Ensure Angular template files are included
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
+  content: [
+    "./src/**/*.{html,ts}", // Ensure Angular template files are included
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
