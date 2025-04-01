f1c3fa61aa7f5481f52eb620f2d2a5d5d4f60e29
Gatt101 <gauravpatilk11@gmail.com>
2025-04-02 00:36:10 +0530

    done almost

--- frontend/src/app/components/about/about.component.ts
+++ frontend/src/app/components/about/about.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {NgxAuroraComponent} from "@omnedia/ngx-aurora";
+import {NgxFadeComponent} from "@omnedia/ngx-fade";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [
+    NgxAuroraComponent,
+    NgxFadeComponent
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
